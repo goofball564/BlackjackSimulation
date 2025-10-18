89474ea2851c609872f60a223de81eac19c7b382
Jack Tackett <jeanvaljean563@gmail.com>
2025-10-18 18:47:51 -0500

    poop

--- main.c
+++ main.c
@@ -2,19 +2,41 @@
 #include "hand.h"
 #include "shoe.h"
 
-int main(void) {
-    Hand hand;
-    init_hand(&hand);
+const char card_chars[13] = {'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'};
 
+int main(void) {
     const int num_decks = 1;
 
     Shoe shoe;
     init_shoe(&shoe, num_decks);
 
-    for (int i = 0; i < CARDS_PER_DECK * num_decks; i++) {
-        const Card c = take_card_shoe(&shoe);
-        printf("Card: %d\n", c);
+    Hand player_hand;
+    init_hand(&player_hand);
+
+    Hand dealer_hand;
+    init_hand(&dealer_hand);
+
+    for (int i = 0; i < 2; i++) {
+        const Card player_card = take_card_shoe(&shoe);
+        add_card_hand(&player_hand, player_card);
+
+        const Card dealer_card = take_card_shoe(&shoe);
+        add_card_hand(&dealer_hand, dealer_card);
     }
+
+    const Card dealer_face_up_card = dealer_hand.cards[0];
+
+
+    // if dealer has blackjack -- lose or push if player has blackjack
+    // else if player has blackjack -- win 3:2
+
+
+
+    // simulated player
+    // places bet based on betting strategy
+    // then plays game based on play strategy (basic, cd exceptions, counting exceptions)
+    // simulate outcome
+    // repeat until player is broke or N hands have been played
 
     return 0;
 }
---
+++ strategy.c
@@ -0,0 +1,6 @@
+#include "strategy.h"
+#include "hand.h"
+
+Decision get_basic_strategy_decision(const Hand* const hand, const Card dealer_face_up_card) {
+
+}
